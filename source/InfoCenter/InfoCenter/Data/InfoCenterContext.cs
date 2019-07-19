885c82b99d639b383ce35a246d79149a1e24a749
oftobcom <oftob.com@gmail.com>
2019-07-19 06:46:38 +0500

    First day
    First day

---
+++ source/InfoCenter/InfoCenter/Data/InfoCenterContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfoCenter.Models
+{
+    public class InfoCenterContext : DbContext
+    {
+        public InfoCenterContext (DbContextOptions<InfoCenterContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<InfoCenter.Models.ClientInfo> ClientInfo { get; set; }
+    }
+}
