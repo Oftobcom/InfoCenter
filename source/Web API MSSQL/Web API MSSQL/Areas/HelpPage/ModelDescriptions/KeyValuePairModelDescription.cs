ec1bdbb66f864ee709f88b1ee95af2a055d2b836
OFTOB.COM <oftob.com@gmail.com>
2019-08-20 10:20:04 +0500

    Revert "Day 4. Web API, MS SQL"
    This reverts commit cbe4d8a06fb95b66d6f2c76de3aaac410abac1e3.

--- source/Web API MSSQL/Web API MSSQL/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_API_MSSQL.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
